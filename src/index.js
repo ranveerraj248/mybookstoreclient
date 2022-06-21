e96bde2019cbfdc3c6a5a55fe4f1ad1a8d92a2c2
Ranveer Raj <ranveerraj248@gmail.com>
2022-06-22 01:25:06 +0530

    downgraded react

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import ReactDOM from "react-dom/client";
+import ReactDom from 'react-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+
+ReactDom.render(<App />, document.getElementById('root'));
